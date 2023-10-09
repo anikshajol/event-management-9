d75181662792831a068094eff86dbbefa99569dc
anikshajol <anikshajol@gmail.com>
2023-10-09 17:39:16 +0600

    loading problem fixed

--- src/Pages/Events/Events.jsx
+++ src/Pages/Events/Events.jsx
@@ -2,32 +2,50 @@ import { useLoaderData } from "react-router-dom";
 import bg from "../../assets/beautiful-photozone-with-big-wreath-decorated-with-greenery-roses-centerpiece-candles-sides-garland-hanged-trees.jpg";
 import Event from "../event/event";
 import Footer from "../Shared/Footer/Footer";
+import { useEffect, useState } from "react";
+import Loading from "../Shared/Loading/Loading";
 
 const Events = () => {
   const events = useLoaderData();
+  const [loader, setLoader] = useState(true);
   // console.log(events);
-  return (
-    <div className="">
-      <h2 className="text-5xl text-center bg-black text-white py-3">
-        {" "}
-        Our Events{" "}
-      </h2>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: `url(${bg})`,
-        }}
-      >
-        <div className="hero-overlay bg-black bg-opacity-80"></div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-2 lg:px-6">
-          {events.map((event) => (
-            <Event key={event.id} event={event}></Event>
-          ))}
+  useEffect(() => {
+    const timing = setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+
+    return () => clearTimeout(timing);
+  }, []);
+
+  return (
+    <>
+      {loader ? (
+        <Loading></Loading>
+      ) : (
+        <div className="">
+          <h2 className="text-5xl text-center bg-black text-white py-3">
+            {" "}
+            Our Events{" "}
+          </h2>
+          <div
+            className="hero min-h-screen"
+            style={{
+              backgroundImage: `url(${bg})`,
+            }}
+          >
+            <div className="hero-overlay bg-black bg-opacity-80"></div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-2 lg:px-6">
+              {events.map((event) => (
+                <Event key={event.id} event={event}></Event>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
+      )}{" "}
       <Footer></Footer>
-    </div>
+    </>
   );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,15 +3,24 @@ import image1 from "../../assets/brides-couple-cutting-wedding-cake.jpg";
 import "./Home.css";
 import Footer from "../Shared/Footer/Footer";
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
-import Loading from "../Shared/Loading/Loading";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Gallery from "../Gallery/Gallery";
+import Loading from "../Shared/Loading/Loading";
 
 const Home = () => {
-  const { user, loading } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    const timing = setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+
+    return () => clearTimeout(timing);
+  }, []);
 
   useEffect(() => {
     AOS.init({
@@ -20,77 +29,77 @@ const Home = () => {
     });
   }, []);
 
-  if (loading) {
-    return <Loading></Loading>;
-  }
-
   return (
     <>
-      <div data-aos="flip-down" className="px-1 md:px-0 overflow-hidden">
-        <div
-          className="hero min-h-screen "
-          style={{
-            backgroundImage: `url(${image1})`,
-          }}
-        >
-          <div className="hero-overlay  bg-black bg-opacity-70"></div>
-          <div className="hero-content text-center text-neutral-content">
-            <div className="max-w-md ">
-              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-              <p className="mb-5">
-                Our One Stop Services are Weddings, Birthday Parties,
-                Anniversaries,Engagement Parties, Retirement Parties, Baby
-                shower, etc.
-              </p>
-              <div className="flex flex-col items-center md:flex-row-reverse justify-center gap-5 ">
-                {!user ? (
-                  <Link to="/register">
-                    <button className=" get-started">Get Started</button>
+      {loader ? (
+        <Loading></Loading>
+      ) : (
+        <div data-aos="flip-down" className="px-1 md:px-0 overflow-hidden">
+          <div
+            className="hero min-h-screen "
+            style={{
+              backgroundImage: `url(${image1})`,
+            }}
+          >
+            <div className="hero-overlay  bg-black bg-opacity-70"></div>
+            <div className="hero-content text-center text-neutral-content">
+              <div className="max-w-md ">
+                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <p className="mb-5">
+                  Our One Stop Services are Weddings, Birthday Parties,
+                  Anniversaries,Engagement Parties, Retirement Parties, Baby
+                  shower, etc.
+                </p>
+                <div className="flex flex-col items-center md:flex-row-reverse justify-center gap-5 ">
+                  {!user ? (
+                    <Link to="/register">
+                      <button className=" get-started">Get Started</button>
+                    </Link>
+                  ) : (
+                    <Link to="/events">
+                      <button className=" get-started">Our Events</button>
+                    </Link>
+                  )}
+                  <Link to={"/about"}>
+                    <button className="primary-btn">About Us</button>
                   </Link>
-                ) : (
-                  <Link to="/events">
-                    <button className=" get-started">Our Events</button>
-                  </Link>
-                )}
-                <Link to={"/about"}>
-                  <button className="primary-btn">About Us</button>
-                </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="  md:px-2 discount-offer flex flex-col justify-center  lg:flex-row md:justify-around items-center ">
-          <div data-aos="flip-down" className="px-4 ">
-            <h2 className="text-white font-semibold text-2xl">
-              <span className="text-4xl font-bold text-[#ffbe30;]">30%</span>{" "}
-              Off In November-December For Wedding Events
+          <div className="  md:px-2 discount-offer flex flex-col justify-center  lg:flex-row md:justify-around items-center ">
+            <div data-aos="flip-down" className="px-4 ">
+              <h2 className="text-white font-semibold text-2xl">
+                <span className="text-4xl font-bold text-[#ffbe30;]">30%</span>{" "}
+                Off In November-December For Wedding Events
+              </h2>
+              <p className="">
+                {" "}
+                Contact us and make your event unique & unforgettable{" "}
+              </p>
+            </div>
+            <div>
+              <button className="offer-btn">Contact us</button>
+            </div>
+          </div>
+
+          {/* gallery section */}
+
+          <div className="mt-4" data-aos="flip-down">
+            <h2 className="text-3xl text-center">
+              Anifa Events Gallery And Our Services
             </h2>
-            <p className="">
-              {" "}
-              Contact us and make your event unique & unforgettable{" "}
-            </p>
-          </div>
-          <div>
-            <button className="offer-btn">Contact us</button>
-          </div>
-        </div>
+            <h2 className="text-5xl font-extrabold text-center pt-2 mb-16">
+              Excellent & Unforgettable Moments
+            </h2>
 
-        {/* gallery section */}
-
-        <div className="mt-4" data-aos="flip-down">
-          <h2 className="text-3xl text-center">
-            Anifa Events Gallery And Our Services
-          </h2>
-          <h2 className="text-5xl font-extrabold text-center pt-2 mb-16">
-            Excellent & Unforgettable Moments
-          </h2>
-
-          <div className="px-1">
-            <Gallery></Gallery>
+            <div className="px-1">
+              <Gallery></Gallery>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       <div>
         <Footer></Footer>
       </div>
