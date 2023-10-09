c7a8882d794c8c7993c982542e3a0bca71e92d92
anikshajol <anikshajol@gmail.com>
2023-10-09 23:20:40 +0600

    a problem fix in login page

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -26,15 +26,7 @@ const Booking = () => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 14 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-            />
-          </svg>
+          ></svg>
           <span className="sr-only">Close menu</span>
         </button>
         <form action="#" className="mb-6">
